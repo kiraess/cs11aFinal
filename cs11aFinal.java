78b8f17631b4121db32daea76ab9990c45f3ddca
kiraess <karrina.xie@gmail.com>
2017-12-03 01:06:04 -0500

    added shuffle and other declartions back in

--- cs11aFinal.java
+++ cs11aFinal.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class cs11aFinal{
   public static void main(String[] args){
 
@@ -130,3 +132,52 @@ public static void bsCaller (){
     int bsCaller = scanner.nextInt();
   }
 }
+
+
+
+/**
+  declaring variable -- Karrina
+*/
+
+public class cs11aFinal{
+  public static void main(String[] args){
+    System.out.println("Hello World");
+    int[] middleSet = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13};
+    int[] player1 = new int[52];
+    int[] player2 = new int[52];
+    int[] player3= new int[52];
+    int[] player4 = new int[52];
+
+    shuffle(middleSet);
+    distribute(middleSet);
+    }
+  }
+
+/**
+  Shuffling the elements in middleSet with Fisher–Yates shuffle
+*/
+  public static void shuffle(int[] middleSet) {
+    int index, temp;
+    Random random = new Random();
+    for (int i = middleSet.length - 1; i > 0; i--)
+    {
+        index = random.nextInt(i + 1);
+        temp = middleSet[index];
+        middleSet[index] = middleSet[i];
+        middleSet[i] = temp;
+    }
+  }
+
+
+
+/**
+  BS Not Effictive
+*/
+  public static void bsFail(int[] middleSet, int[] player1, int[] player2, int[] player3, int[] player4){
+    //use instance method would be hella easier
+  }
+
+}
+
+/* for (int i = 0; i < middleSet.length; i++){
+     System.out.print(middleSet[i] + " ");
